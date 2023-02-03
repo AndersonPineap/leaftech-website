6f2b7db9103ae6abb2539d0695737bed9ec922db
flowe <flowerlife85@qq.com>
2023-02-03 12:02:22 +0800

    update

--- creatAccount.py
+++ creatAccount.py
@@ -1,4 +1,5 @@
 import pymongo
+from datetime import datetime
 from random import randint
 from hashlib import md5
 usr = input("用户名：")
@@ -14,15 +15,18 @@ try:
     database.user.insert_one({
         'username': usr,
         'password': md5(pwd.encode("utf-8")).hexdigest(),
-        'admin': False,
+        'admin': True,
         'uplaod-article': [],
         'upload-code-pre': [],
         'task': [],
-        'avatar': 'default.jpg'
+        'avatar': 'default.jpg',
+        'level': datetime.today().year
     })
 except:
     print("失败，我也不知道为什么")
 print("done")
 print(f"账户：{usr}")
 print("密码md5："+md5("password".encode("utf-8")).hexdigest())
+print("默认为管理员（可在代码中修改）")
+print(f"账户level:{datetime.today().year}")
 print("萨哟那拉，bye")
